2911580f04fd2ba8962785ace351b303e3dc5be9
Thomas Lee <lisihan969@gmail.com>
2016-02-24 00:13:14 +0800

    Implement filesParser.

--- adapter/cic/parser.go
+++ adapter/cic/parser.go
@@ -6,11 +6,21 @@ import (
 	"github.com/tsinghua-io/api-server/resource"
 	"io"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type parser interface {
 	parse(reader io.Reader, info interface{}, langCode string) error
 }
+
+func parseRegDate(regDate int64) string {
+	return time.Unix(regDate%1000, 0).Format("2006-01-02T15:04:05+0800")
+}
+
+const (
+	downloadURL = "http://learn.cic.tsinghua.edu.cn/b/resource/downloadFileStream/{file_id}"
+)
 
 type personalInfoParser struct {
 	DataSingle struct {
@@ -254,26 +264,69 @@ func (p *announcementsParser) parse(r io.Reader, info interface{}, _ string) err
 }
 
 type filesParser struct {
-	resultList map[string]struct {
-		teacherInfoView struct {
-			nodeName     string
-			childMapData map[string]struct {
-				title                string
-				courseCoursewareList []struct {
-					resourcesMappingByFileId struct {
-						fileId   string
-						regDate  int64
-						fileName string
-						fileSize string
-						userCode string
+	ResultList map[string]struct {
+		TeacherInfoView struct {
+			NodeName     string
+			ChildMapData map[string]struct {
+				Title                string
+				CourseCoursewareList []struct {
+					ResourcesMappingByFileId struct {
+						FileId   string
+						RegDate  int64
+						FileName string
+						FileSize string
+						CourseId string
+						UserCode string
 					}
-					regUser string
-					title   string
-					detail  string
+					RegUser string
+					Title   string
+					Detail  string
 				}
 			}
 		}
 	}
+}
+
+func (p *filesParser) parse(r io.Reader, info interface{}, _ string) error {
+	files, ok := info.(*[]*resource.File)
+	if !ok {
+		return fmt.Errorf("The parser and the destination type do not match.")
+	}
+
+	dec := json.NewDecoder(r)
+	if err := dec.Decode(p); err != nil {
+		return err
+	}
+
+	for _, node1 := range p.ResultList {
+		for _, node2 := range node1.TeacherInfoView.ChildMapData {
+			category := []string{node1.TeacherInfoView.NodeName, node2.Title}
+
+			for _, item := range node2.CourseCoursewareList {
+				fileId := item.ResourcesMappingByFileId.FileId
+				size, _ := strconv.Atoi(item.ResourcesMappingByFileId.FileSize)
+
+				file := &resource.File{
+					Id:       fileId,
+					CourseId: item.ResourcesMappingByFileId.CourseId,
+					Owner: resource.User{
+						Id:   item.ResourcesMappingByFileId.UserCode,
+						Name: item.RegUser,
+					},
+					CreatedAt:   parseRegDate(item.ResourcesMappingByFileId.RegDate),
+					Title:       item.Title,
+					Description: item.Detail,
+					Category:    category,
+					Filename:    item.ResourcesMappingByFileId.FileName,
+					Size:        size,
+					DownloadUrl: strings.Replace(downloadURL, "{file_id}", fileId, -1),
+				}
+				*files = append(*files, file)
+			}
+		}
+	}
+
+	return nil
 }
 
 type homeworksParser struct {
