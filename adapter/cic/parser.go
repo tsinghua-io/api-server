c7d69374a02ac2c77372f3df2d079dbd5ffa06e7
Thomas Lee <lisihan969@gmail.com>
2016-02-28 15:52:05 +0800

    Fix a bug in parseRegDate

--- adapter/cic/parser.go
+++ adapter/cic/parser.go
@@ -10,5 +10,5 @@ type parser interface {
 }
 
 func parseRegDate(regDate int64) string {
-	return time.Unix(regDate%1000, 0).Format("2006-01-02T15:04:05+0800")
+	return time.Unix(regDate/1000, 0).Format("2006-01-02T15:04:05+0800")
 }
