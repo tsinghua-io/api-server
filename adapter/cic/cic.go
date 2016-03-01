576e694a7fc7ec8b44fd956032a08c2cb792e2a6
Thomas Lee <lisihan969@gmail.com>
2016-03-01 21:29:16 +0800

    Cleanup cic

--- adapter/cic/cic.go
+++ adapter/cic/cic.go
@@ -2,20 +2,23 @@ package cic
 
 import (
 	"github.com/golang/glog"
+	"io"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"strings"
 	"time"
+)
+
+const (
+	BaseURL     = "http://learn.cic.tsinghua.edu.cn"
+	DownloadURL = BaseURL + "/b/resource/downloadFileStream/{file_id}"
 )
 
 type CicAdapter struct {
 	client   http.Client
 	LangCode string
 }
-
-const (
-	BaseURL = "http://learn.cic.tsinghua.edu.cn"
-)
 
 var parsedBaseURL, _ = url.Parse(BaseURL)
 
@@ -72,4 +75,23 @@ func (adapter *CicAdapter) FetchInfo(url string, method string, p parser, info i
 
 	// We are safe.
 	return http.StatusOK
+}
+
+// Helper functions.
+
+type parser interface {
+	parse(reader io.Reader, info interface{}, langCode string) error
+}
+
+func parseRegDate(regDate int64) string {
+	// Return empty string for 0.
+	// Will not work for 1970-01-01T08:00:00+0800.
+	if regDate == 0 {
+		return ""
+	}
+	return time.Unix(regDate/1000, 0).Format("2006-01-02T15:04:05+0800")
+}
+
+func fileID2DownloadUrl(fileID string) string {
+	return strings.Replace(DownloadURL, "{file_id}", fileID, -1)
 }
