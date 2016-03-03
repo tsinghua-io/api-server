1b9bd812cdf58cd48c4895cbe320ebdfb889d86f
Thomas Lee <lisihan969@gmail.com>
2016-03-03 20:48:21 +0800

    Parallelize old annc.

--- adapter/old/announcement.go
+++ adapter/old/announcement.go
@@ -27,64 +27,86 @@ func (adapter *OldAdapter) announcementBody(path string) (body string) {
 	return
 }
 
+func (adapter *OldAdapter) parseRow(s *goquery.Selection, courseId string, annc **resource.Announcement) error {
+	infos := s.Find("td").Map(func(i int, tdSelection *goquery.Selection) (info string) {
+		info, _ = tdSelection.Html()
+		return
+	})
+
+	hrefSelection := s.Find("td a")
+
+	var href string
+	var hrefUrl *url.URL
+	var err error
+	href, _ = hrefSelection.Attr("href")
+
+	if hrefUrl, err = url.Parse(href); err != nil {
+		return err
+	}
+
+	if announcementId := hrefUrl.Query().Get("id"); announcementId != "" {
+		body := adapter.announcementBody(href)
+
+		var priority int
+		var title string
+		switch hrefSelection.Nodes[0].FirstChild.Type {
+		case html.TextNode:
+			priority = 0
+			title, _ = hrefSelection.Html()
+		default:
+			priority = 1
+			title, _ = hrefSelection.Children().Html()
+		}
+
+		*annc = &resource.Announcement{
+			Id:       announcementId,
+			CourseId: courseId,
+
+			Owner: &resource.User{
+				Name: infos[2],
+			},
+			CreatedAt: infos[3],
+			Priority:  priority,
+			Read:      true, // We just read it.
+
+			Title: title,
+			Body:  body,
+		}
+		return nil
+	} else {
+		return fmt.Errorf("Cannot get announcement_id from %s", hrefUrl)
+	}
+}
+
 func (adapter *OldAdapter) Announcements(courseId string) (announcements []*resource.Announcement, status int) {
 	path := "/MultiLanguage/public/bbs/getnoteid_student.jsp?course_id=" + courseId
 	doc, err := adapter.getOldResponse(path, make(map[string]string))
 
-	status = http.StatusBadGateway
-
 	if err != nil {
 		glog.Errorf("Failed to get response from learning web: %s", err)
-	} else {
-		trs := doc.Find("tr.tr1, tr.tr2")
-		trs.Each(func(i int, s *goquery.Selection) {
-			infos := s.Find("td").Map(func(i int, tdSelection *goquery.Selection) (info string) {
-				info, _ = tdSelection.Html()
-				return
-			})
-
-			hrefSelection := s.Find("td a")
-
-			var href string
-			var hrefUrl *url.URL
-			var err error
-			href, _ = hrefSelection.Attr("href")
-
-			if hrefUrl, err = url.Parse(href); err != nil {
-				return
-			}
-
-			if announcementId := hrefUrl.Query().Get("id"); announcementId != "" {
-				body := adapter.announcementBody(href)
-
-				var priority int
-				var title string
-				switch hrefSelection.Nodes[0].FirstChild.Type {
-				case html.TextNode:
-					priority = 0
-					title, _ = hrefSelection.Html()
-				default:
-					priority = 1
-					title, _ = hrefSelection.Children().Html()
-				}
-
-				announcements = append(announcements, &resource.Announcement{
-					Id:       announcementId,
-					CourseId: courseId,
-
-					Owner: &resource.User{
-						Name: infos[2],
-					},
-					CreatedAt: infos[3],
-					Priority:  priority,
-					Read:      true, // We just read it.
-
-					Title: title,
-					Body:  body,
-				})
-			}
-		})
-		status = http.StatusOK
+		return nil, http.StatusBadGateway
 	}
-	return
+
+	trs := doc.Find("tr.tr1, tr.tr2")
+	count := trs.Size()
+	announcements = make([]*resource.Announcement, count)
+	errChan := make(chan error, count)
+
+	i := 0
+	trs.Each(func(_ int, s *goquery.Selection) {
+		index := i
+		go func() {
+			errChan <- adapter.parseRow(s, courseId, &announcements[index])
+		}()
+		i++
+	})
+
+	for i = 0; i < count; i++ {
+		if err := <-errChan; err != nil {
+
+			return nil, http.StatusBadGateway
+		}
+	}
+
+	return announcements, http.StatusOK
 }
