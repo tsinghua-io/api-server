1f0e616101feeb28f26b8a88e7c478d5a70b00b0
Thomas Lee <lisihan969@gmail.com>
2016-03-08 10:59:56 +0800

    Implement mixed adapter

--- adapter/mixed/mixed.go
+++ adapter/mixed/mixed.go
@@ -1,30 +1,67 @@
 package mixed
 
 import (
+	"github.com/tsinghua-io/api-server/adapter/cic"
+	"github.com/tsinghua-io/api-server/adapter/old"
 	"github.com/tsinghua-io/api-server/resource"
 	"net/http"
+	"strings"
 )
 
 type MixedAdapter struct {
+	old old.OldAdapter
+	cic cic.CicAdapter
 }
 
-func Login(username string, password string) (cookies []*http.Cookie, err error) {
+func (ada *MixedAdapter) Profile(username string, params map[string]string) (user *resource.User, status int) {
+	return ada.cic.Profile(username, params)
 }
 
-func (adapter *MixedAdapter) PersonalInfo() (user *resource.User, status int) {
+func (ada *MixedAdapter) Attended(username string, params map[string]string) (courses []*resource.Course, status int) {
+	cic2old := make(chan map[string]string, 1)
+
+	go func() {
+		courses, status := ada.old.Attended(username, params)
+		if status != http.StatusOK {
+			cic2old <- make(map[string]string)
+		}
+		cic2old <- old.CourseIdMap(courses)
+	}()
+
+	if courses, status = ada.cic.Attended(username, params); status != http.StatusOK {
+		return nil, status
+	}
+
+	// We are safe, substitute ids.
+	mapping := <-cic2old
+	for _, course := range courses {
+		if oldId := mapping[course.Id]; oldId != "" {
+			course.Id = oldId
+		}
+	}
+	return courses, http.StatusOK
 }
 
-func (adapter *MixedAdapter) Attending() (courses []*resource.Course, status int) {
+func (ada *MixedAdapter) CourseAnnouncements(courseId string, params map[string]string) (announcements []*resource.Announcement, status int) {
+	if strings.Contains(courseId, "-") {
+		return ada.cic.CourseAnnouncements(courseId, params)
+	} else {
+		return ada.old.CourseAnnouncements(courseId, params)
+	}
 }
 
-func (adapter *MixedAdapter) Attended() (courses []*resource.Course, status int) {
+func (ada *MixedAdapter) CourseFiles(courseId string, params map[string]string) (files []*resource.File, status int) {
+	if strings.Contains(courseId, "-") {
+		return ada.cic.CourseFiles(courseId, params)
+	} else {
+		return ada.old.CourseFiles(courseId, params)
+	}
 }
 
-func (adapter *MixedAdapter) Announcements(courseId string) (courses []*resource.Announcement, status int) {
-}
-
-func (adapter *MixedAdapter) Files(courseId string) (courses []*resource.File, status int) {
-}
-
-func (adapter *MixedAdapter) Homeworks(courseId string) (courses []*resource.Homework, status int) {
+func (ada *MixedAdapter) CourseHomework(courseId string, params map[string]string) (files []*resource.Homework, status int) {
+	if strings.Contains(courseId, "-") {
+		return ada.cic.CourseHomework(courseId, params)
+	} else {
+		return ada.old.CourseHomework(courseId, params)
+	}
 }
